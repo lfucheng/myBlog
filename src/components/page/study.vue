bc84049c662cbfd43e825f05117b1b10767b9227
lfucheng <1462168380@qq.com>
2017-09-08 16:51:52 +0800

    study  ..

---
+++ src/components/page/study.vue
@@ -0,0 +1,192 @@
+<template>
+    <div class="study">
+        <div class="layer">
+            <div class="header">
+              <div class="wrapper">
+                <span>李福成的博客</span>
+                <ul>
+                  <li>首页</li>
+                  <li>学海无涯</li>
+                  <li>我的爱好</li>
+                  <li>个人日记</li>
+                  <li>二次元</li>
+                  <li>关于我</li>
+                </ul>
+              </div>
+            </div>
+            <div class="nav">
+                
+            </div>
+            <div class="wrapper">
+              <div class="content">
+                <h5 class="page-head">首页 > 学海无涯 > 最新文章</h5>
+                <div class="con-left">
+                  <ul class="con-left-ul">
+                    <li>
+                      <h4>一个前端的自我修养（转）</h4>
+                      <img src="../../assets/img/1-161211200G00-L.jpg">
+                      <div class="author-info">
+                        <span class="glyphicon glyphicon-time"></span>&nbsp;
+                        <p>发布时间：2017-06-06 | </p>
+                        <span class="glyphicon glyphicon-folder-open"></span>&nbsp;
+                        <p>分类于：HTML | </p>
+                        <span class="glyphicon glyphicon-eye-open"></span>&nbsp;
+                        <p>阅读数：134</p>
+                      </div>
+                      <div class="news-info">
+                        <p>
+                          前言：看过我之前写的系列文章（Vue.js基础篇、Vue.js组件篇)，相信大家对vue.js这个前端框架有了一定的了解。想必也想把Vue急切的运用在项目中，看看它的魅力到底有多大？别急，今天我会满足大家的想法。我们一起来看看“Webpack+Vue”的开发模式相比以往老项目(Gulp+jQuery)的开发模式的魅力在哪里。
+                        </p>
+                      </div>
+                      <a href="">阅读全文 >></a>
+                    </li>
+                    <li>
+                      <h4>一个前端的自我修养（转）</h4>
+                      <img src="../../assets/img/1-161211200G00-L.jpg">
+                      <div class="author-info">
+                        <span class="glyphicon glyphicon-time"></span>&nbsp;
+                        <p>发布时间：2017-06-06 | </p>
+                        <span class="glyphicon glyphicon-folder-open"></span>&nbsp;
+                        <p>分类于：HTML | </p>
+                        <span class="glyphicon glyphicon-eye-open"></span>&nbsp;
+                        <p>阅读数：134</p>
+                      </div>
+                      <div class="news-info">
+                        <p>
+                          前言：看过我之前写的系列文章（Vue.js基础篇、Vue.js组件篇)，相信大家对vue.js这个前端框架有了一定的了解。想必也想把Vue急切的运用在项目中，看看它的魅力到底有多大？别急，今天我会满足大家的想法。我们一起来看看“Webpack+Vue”的开发模式相比以往老项目(Gulp+jQuery)的开发模式的魅力在哪里。
+                        </p>
+                      </div>
+                      <a href="">阅读全文 >></a>
+                    </li>
+                    <li>
+                      <h4>一个前端的自我修养（转）</h4>
+                      <img src="../../assets/img/1-161211200G00-L.jpg">
+                      <div class="author-info">
+                        <span class="glyphicon glyphicon-time"></span>&nbsp;
+                        <p>发布时间：2017-06-06 | </p>
+                        <span class="glyphicon glyphicon-folder-open"></span>&nbsp;
+                        <p>分类于：HTML | </p>
+                        <span class="glyphicon glyphicon-eye-open"></span>&nbsp;
+                        <p>阅读数：134</p>
+                      </div>
+                      <div class="news-info">
+                        <p>
+                          前言：看过我之前写的系列文章（Vue.js基础篇、Vue.js组件篇)，相信大家对vue.js这个前端框架有了一定的了解。想必也想把Vue急切的运用在项目中，看看它的魅力到底有多大？别急，今天我会满足大家的想法。我们一起来看看“Webpack+Vue”的开发模式相比以往老项目(Gulp+jQuery)的开发模式的魅力在哪里。
+                        </p>
+                      </div>
+                      <a href="">阅读全文 >></a>
+                    </li>
+                    <li>
+                      <h4>一个前端的自我修养（转）</h4>
+                      <img src="../../assets/img/1-161211200G00-L.jpg">
+                      <div class="author-info">
+                        <span class="glyphicon glyphicon-time"></span>&nbsp;
+                        <p>发布时间：2017-06-06 | </p>
+                        <span class="glyphicon glyphicon-folder-open"></span>&nbsp;
+                        <p>分类于：HTML | </p>
+                        <span class="glyphicon glyphicon-eye-open"></span>&nbsp;
+                        <p>阅读数：134</p>
+                      </div>
+                      <div class="news-info">
+                        <p>
+                          前言：看过我之前写的系列文章（Vue.js基础篇、Vue.js组件篇)，相信大家对vue.js这个前端框架有了一定的了解。想必也想把Vue急切的运用在项目中，看看它的魅力到底有多大？别急，今天我会满足大家的想法。我们一起来看看“Webpack+Vue”的开发模式相比以往老项目(Gulp+jQuery)的开发模式的魅力在哪里。
+                        </p>
+                      </div>
+                      <a href="">阅读全文 >></a>
+                    </li>
+                  </ul>
+                  
+                  <nav aria-label="Page navigation">
+                    <ul class="pagination">
+                      <li>
+                        <a href="#" aria-label="Previous">
+                          <span aria-hidden="true">&laquo;</span>
+                        </a>
+                      </li>
+                      <li><a href="#">1</a></li>
+                      <li><a href="#">2</a></li>
+                      <li><a href="#">3</a></li>
+                      <li><a href="#">4</a></li>
+                      <li><a href="#">5</a></li>
+                      <li>
+                        <a href="#" aria-label="Next">
+                          <span aria-hidden="true">&raquo;</span>
+                        </a>
+                      </li>
+                    </ul>
+                  </nav>
+                </div>
+                <div class="con-right">
+                  <div class="search-box">
+                    <input type="text" name="" class="search-txt" placeholder="请输入你需要的内容">
+                    <!-- <input type="button" name="" class="search-btn"> -->
+                    <span class="glyphicon glyphicon-search search-btn"></span>
+                  </div>
+                  <div class="new-box">
+                    <div class="con-right-title">
+                      <span class="glyphicon glyphicon-tags"></span>&nbsp;&nbsp;
+                      <h4>最新文章</h4>
+                    </div>
+                    <ul>
+                      <li>三步实现滚动条触动css动画效果现滚动条触动css动画效果</li>
+                      <li>请不要在设计这条路上徘徊啦</li>
+                      <li>一个前端的自我修养（转）</li>
+                      <li>设计的那点事</li>
+                      <li>技术真的有那么重要吗？</li>
+                      <li>学习网页制作我能干什么？</li>
+                      <li>学习网页制作我能干什么？</li>
+                      <li>学习网页制作我能干什么？</li>
+                      <li>技术真的有那么重要吗？</li>
+                      <li>学习网页制作我能干什么？</li>
+                      <li>学习网页制作我能干什么？</li>
+                      <li>学习网页制作我能干什么？</li>
+                    </ul>
+                  </div>
+                  <div class="contact-box">
+                    <div class="con-right-title">
+                     <span class="glyphicon glyphicon-headphones"></span>&nbsp;&nbsp;
+                      <h4>联系我</h4>
+                    </div>
+                    <div class="contact-con">
+                      <i class="iconfont qq">&#xe608;</i>
+                      <span>: 1462168380</span>
+                      <br />
+                      <i class="iconfont weixin">&#xe65b;</i>
+                      <span>: lfc1462168380</span>
+                      <br />
+                      <i class="iconfont weibo">&#xe600;</i>
+                      <span>: 无聊</span>
+                      <br />
+                      <span class="email"><strong>Email</strong>: 1462168380@qq.com</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            
+            </div>
+            
+        </div>
+        
+    </div>
+</template>
+
+<script>
+
+// import CommonHeader from '../common/CommonHeader'
+
+export default {
+  name: 'study',
+  data () {
+    return {
+    }
+  },
+  components:{
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import "../../assets/css/study.css";
+@import "../../assets/font/iconfont.css";
+</style>
